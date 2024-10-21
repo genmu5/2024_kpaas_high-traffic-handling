bbc50d6109b016f04193890e6f43f0fddc0fc756
JunHyukJang426 <joonhyeok426624@gmail.com>
2024-10-21 22:41:55 +0900

    DisasterScheduler 수정

--- 2024_kpaas/kpass-spring-master/src/main/java/com/example/kpass/disastermanagement/scheduler/DisasterScheduler.java
+++ 2024_kpaas/kpass-spring-master/src/main/java/com/example/kpass/disastermanagement/scheduler/DisasterScheduler.java
@@ -4,6 +4,7 @@ package com.example.kpass.disastermanagement.scheduler;
 import com.example.kpass.disastermanagement.controller.DisasterController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory; ////
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +15,15 @@ public class DisasterScheduler {
 
     private final DisasterController disasterController;
 
+    @Value("${scheduler.fixedRate:90000}")
+    private long fixedRate;
+
     public DisasterScheduler(DisasterController disasterController) {
         this.disasterController = disasterController;
     }
 
     // 1.5분(90초)마다 getDisasters 메서드 호출
-    @Scheduled(fixedRate = 90000)
+    @Scheduled(fixedRateString = "${scheduler.fixedRate}")
     public void scheduleDisasterFetch() {
         logger.info("스케줄러가 API를 호출합니다.");
         disasterController.getDisasters();
